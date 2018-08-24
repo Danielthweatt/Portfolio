7bce1cdd25c9d5c3e9d42322a3cfcc0ad2c667a2
Danielthweatt <dthweatt192@yahoo.com>
2018-08-23 22:58:24 -0500

    worked on linter, manifest.json, App.js, and created Nav component

---
+++ my-app/src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => (
+	<nav>
+		<h1>Daniel Thweatt</h1>
+		<a href="https://github.com/Danielthweatt/" target="_blank">GitHub</a>
+		<a href="https://www.linkedin.com/in/daniel-thweatt-21718114b/" target="_blank">LinkedIn</a>
+	</nav>
+);
+
+export default Nav;

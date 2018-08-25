35e9aef3d40355914ac05c516f705907f6a3cd97
Danielthweatt <dthweatt192@yahoo.com>
2018-08-24 19:32:52 -0500

    created Wrapper component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
+import Wrapper from './components/Wrapper';
 
 class App extends Component {
 	render() {
 		return (
-			<Nav />
+			<div>
+				<Nav />
+				<main>
+					<Wrapper>
+						<h1>
+							Hi
+						</h1>
+					</Wrapper>
+				</main>
+			</div>
 		);
 	}
 }

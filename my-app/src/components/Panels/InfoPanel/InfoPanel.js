436aca970e08353eaed4829ec4ca67cf91a9a093
Danielthweatt <dthweatt192@yahoo.com>
2018-09-08 23:53:14 -0500

    worked on panels

--- my-app/src/components/Panels/InfoPanel/InfoPanel.js
+++ my-app/src/components/Panels/InfoPanel/InfoPanel.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './InfoPanel.css';
 
 const InfoPanel = () => (
-	<div className="info-panel"></div>
+	<div className="info-panel">
+		<h3>Hello</h3>
+		<hr />
+		<p>Lorem ipsum</p>
+	</div>
 );
 
 export default InfoPanel;

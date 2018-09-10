ceb41989c6714c14e88cd64040c9daf7856e5ce7
Danielthweatt <dthweatt192@yahoo.com>
2018-09-09 23:59:34 -0500

    worked on panels

--- my-app/src/components/Panels/InfoPanel/InfoPanel.js
+++ my-app/src/components/Panels/InfoPanel/InfoPanel.js
@@ -3,9 +3,11 @@ import './InfoPanel.css';
 
 const InfoPanel = () => (
 	<div className="info-panel">
-		<h3>Hello</h3>
-		<hr />
-		<p>Lorem ipsum</p>
+		<div className="info">
+			<h3>Hello</h3>
+			<hr />
+			<p>Lorem ipsum</p>
+		</div>
 	</div>
 );
 

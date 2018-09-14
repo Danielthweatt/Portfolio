0b2489f70e757fe6f2cea8d296080fdbe6a61d7a
Danielthweatt <dthweatt192@yahoo.com>
2018-09-14 08:05:46 -0500

    worked on About component, nav links, and info panel

--- my-app/src/components/About/About.js
+++ my-app/src/components/About/About.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './About.css';
 
 const About = () => (
-    <div>
-        <h2>About Me</h2>
-        <hr />
-        <p>Lorem ipsum</p>
-    </div>
+	<div>
+		<h2>About Me</h2>
+		<hr />
+		<p>Life really is like a box of chocolates. You 
+        never know what you're going to get! Or at least that 
+        has been the case for me. Luckily, I enjoy 
+        new challenges, new opportunities to grow and develop, 
+        and so I keep loving life despite its twists 
+        and turns.</p>
+		<p>Case in point, I am currently learning how to code 
+        and develop web pages; one of those life journeys 
+        I never thought I would be on! I am loving every 
+        second of it, though. Check out some of my projects, one 
+        of which is this webpage, in my portfolio!</p>
+	</div>
 );
 
 export default About;

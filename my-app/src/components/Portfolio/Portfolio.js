83b265a7913d1896f08d1fbeeeb3f14e5f10b87b
Danielthweatt <dthweatt192@yahoo.com>
2018-09-16 21:23:12 -0500

    worked on info panel, portfolio, and project components

--- my-app/src/components/Portfolio/Portfolio.js
+++ my-app/src/components/Portfolio/Portfolio.js
@@ -4,17 +4,51 @@ import Project from '../Project';
 
 class Portfolio extends Component {
     state = {
-        project: {
-            
-        }
+        projects: [{
+            title: "Project 1",
+            image: "An image",
+            description: "A description",
+            liveLink: "A live link",
+            gitHubLink: "A GitHub link"
+        },
+        {
+            title: "Project 2",
+            image: "Another image",
+            description: "Another description",
+            liveLink: "Another live link",
+            gitHubLink: "Another GitHub link"
+        }],
+        index: 0
     };
+
+    previous = () => {
+        if (this.state.index > 0) {
+            this.setState((prevState) => ({
+                index: prevState.index - 1
+            }));
+        }
+    }
+
+    next = () => {
+        if (this.state.index < this.state.projects.length - 1) {
+            this.setState((prevState) => ({
+                index: prevState.index + 1
+            }));
+        }
+    }
     
     render() {
 		return (
 			<React.Fragment>
 				<h2>Portfolio</h2>
 		        <hr />
-			    <Project />
+			    <Project projectInfo={this.state.projects[this.state.index]} />
+                <button onClick={this.previous}>
+                    Previous
+                </button>
+                <button onClick={this.next}>
+                    Next
+                </button>
 			</React.Fragment>
 		);
 	}
--- my-app/src/components/Project/Project.js
+++ my-app/src/components/Project/Project.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Project.css';
 
-const Project = () => (
-	<p>Lorem ipsum dollar sit amet</p>
+const Project = props => (
+	<React.Fragment>
+		<h3>{props.projectInfo.title}</h3>
+	    <p>{props.projectInfo.image}</p>
+		<p>{props.projectInfo.description}</p>
+		<p>{props.projectInfo.liveLink}</p>
+		<p>{props.projectInfo.gitHubLink}</p>
+	</React.Fragment>
 );
 
 export default Project;
